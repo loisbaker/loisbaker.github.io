1909d73bd7ee72faec1fef5d3cfd897e9e288791
Lois Baker <43139023+loisbaker@users.noreply.github.com>
2021-11-22 10:34:57 +0000

    build website

--- js/wowchemy-slides.js
+++ js/wowchemy-slides.js
@@ -1,6 +1,6 @@
 (() => {
   // ns-params:@params
-  var slides = {highlight_style: "dracula", theme: "black"};
+  var slides = { highlight_style: "dracula", theme: "black" };
 
   // ns-hugo:/var/folders/g7/hf0s64x16n177g6rg3rt6gfm0000gn/T/hugo_cache/modules/filecache/modules/pkg/mod/github.com/wowchemy/wowchemy-hugo-modules/wowchemy@v0.0.0-20210425191248-0afcc81296de/assets/js/wowchemy-utils.js
   function fixMermaid(render = false) {
